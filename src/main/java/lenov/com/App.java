9f28ccd191e49c5cdbfd1b2929316ed45aeaa3e6
root <root@ip-172-31-23-253.us-east-2.compute.internal>
2019-12-21 09:59:31 +0000

    lenov.com

---
+++ src/main/java/lenov/com/App.java
@@ -0,0 +1,13 @@
+package lenov.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
